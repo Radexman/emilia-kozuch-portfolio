67523330d7e6aa507b6a106601f686c1c869786a
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2023-02-10 13:16:10 +0100

    02.10

--- dist/app.js
+++ dist/app.js
@@ -2,21 +2,25 @@ const hamburger = document.querySelector('.hamburger');
 const navlinks = document.querySelector('.navlinks');
 const navlink = document.querySelectorAll('.navlinks__navlink');
 const accordion = document.getElementsByClassName('accordion__box');
+const acreditation = document.querySelector('.footer__date');
+const year = new Date().getFullYear();
+const finalYear = ` ${year}`;
+acreditation.textContent += finalYear;
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger--active');
-    navlinks.classList.toggle('navlinks--active');
-})
+	hamburger.classList.toggle('hamburger--active');
+	navlinks.classList.toggle('navlinks--active');
+});
 
 navlink.forEach((link) => {
-    link.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger--active');
-        navlinks.classList.toggle('navlinks--active');
-    })
-})
+	link.addEventListener('click', () => {
+		hamburger.classList.toggle('hamburger--active');
+		navlinks.classList.toggle('navlinks--active');
+	});
+});
 
 for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener('click', function() {
-        this.classList.toggle('active');
-    })
+	accordion[i].addEventListener('click', function () {
+		this.classList.toggle('active');
+	});
 }
